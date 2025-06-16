57dd8ba32069a826916d11a44296c710684dceca
Daniel <danolox@gmail.com>
2025-06-16 10:02:53 +0000

    cambios para entrega

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,59 +6,51 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = ({digito1, digito2, digito3, digito4, digito5, digito6}) => {
 	return (
-		<div className="text-center bg-dark">
+		<div className="bg-dark">
             
-			<div className="container d-flex">
+			<div className="container text-center d-flex justify-content-center mt-5 gap-3">
 
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito6}</h5>
 					</div>
 				</div>
 
 				
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito5}</h5>
 					</div>
 				</div>
 
 
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito4}</h5>
 					</div>
 				</div>
 
 
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito3}</h5>
 					</div>
 				</div>
 
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito2}</h5>
 					</div>
 				</div>
 
 
-				<div className="card bg-secondary" style={{width: "18rem", color: "white"}}>
+				<div className="card bg-secondary" style={{width: "10rem", color: "white"}}>
 					<div className="card-body">
 						<h5 className="card-title">{digito1}</h5>
 					</div>
 				</div>
 
 			</div>
-			
-
-
-			{/* <div style={{width: "18rem", color: "white", borderRadius: "10px"}}>
-				<h5 className="card-title">Contador</h5>
-				<p className="card-text">{digito1}</p>
-			</div> */}
-
 			
 		</div>
 	);
